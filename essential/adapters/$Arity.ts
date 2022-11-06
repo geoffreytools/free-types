b31cf2dd5c21d439ee1b5c61d4eea2e6065c03d8
geoffreytools <29453961+geoffreytools@users.noreply.github.com>
2022-11-06 14:25:56 +0100

    fix: make $Arity support optional params

--- essential/adapters/$Arity.ts
+++ essential/adapters/$Arity.ts
@@ -1,5 +1,5 @@
 import { apply, Slice, Type } from "free-types-core";
 
 export interface $Arity<N extends number, $T extends Type>
-    extends Type<Slice<$T['constraints'], 0, N>>
+    extends Type<Slice<Required<$T['constraints']>, 0, N>>
         { type: apply<$T, this['arguments']> }
--- tests/essential/adapters/$Arity.ts
+++ tests/essential/adapters/$Arity.ts
@@ -3,12 +3,15 @@ import { Type, apply } from 'free-types-core';
 import { $Arity } from '../../../essential/adapters/$Arity';
 
 interface $FirstArg extends Type { type: this['arguments'][0] }
+interface $OptionalArg extends Type<[unknown, unknown?]> { type: this['arguments'][1] }
 
 type $FirstArgOf2 = $Arity<2, $FirstArg>;
+type $OptionalArgOf2 = $Arity<2, $OptionalArg>;
 
-test('$Arity sets the arity' as const, t =>
-    t.equal<$FirstArgOf2['constraints']['length'], 2>()
-)
+test('$Arity sets the arity' as const, t => [
+    t.equal<$FirstArgOf2['constraints']['length'], 2>(),
+    t.equal<$OptionalArgOf2['constraints']['length'], 2>(),
+])
 
 /* use case */
 
@@ -17,5 +20,11 @@ type Foo<$T extends Type<2>> = apply<$T, [1, 2]>;
 // @ts-expect-error: control (Type doesn't extend Type<2>)
 type Rejected = Foo<$FirstArg>
 
+// @ts-expect-error: control (Type doesn't extend Type<2>)
+type Rejected2 = Foo<$OptionalArg>
+
 // $Arity sets the arity
 type Accepted = Foo<$FirstArgOf2>
+
+// $Arity sets the arity
+type Accepted2 = Foo<$OptionalArgOf2>
