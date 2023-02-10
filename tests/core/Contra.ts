598719c986a34595365966c0dbf24ce02cb30cfc
geoffreytools <29453961+geoffreytools@users.noreply.github.com>
2023-02-10 19:28:40 +0100

    fix (Contra): missing Type in type constraint

--- tests/core/Contra.ts
+++ tests/core/Contra.ts
@@ -1,4 +1,4 @@
-import { Contra, Type } from '../../core'
+import { Contra, Type, apply, free } from '../../core'
 import { $Add } from '../../utility-types'
 
 type ContraNarrower<$T extends Contra<$T, Type<[1, 2]>>> = null
@@ -10,3 +10,11 @@ type ContraWider<$T extends Contra<$T, Type<2>>> = null
 
 // @ts-expect-error: A more specific Type can't be passed to a HKT
 type TooNarrow = ContraWider<$Add>
+
+
+//application
+
+type ApplyTo42<$T extends Contra<$T, Type<[42]>>> = apply<$T, [42]>
+
+type foo = ApplyTo42<$Add<2>>
+type bar = ApplyTo42<free.Set>;
