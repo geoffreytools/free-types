42c55d5f461a48a0372a4d737a7521318788a4d6
geoffreyTools <geoffreyTools@users.noreply.github.com>
2023-06-28 09:31:11 +0200

    fix: make `Mappable` accept readonly arrays
    It should make no difference, as everything is readonly at the type level, but it is more convenient when using utils such as MapOver

--- src/essential/mappables/common.ts
+++ src/essential/mappables/common.ts
@@ -1,6 +1,6 @@
 export type Key = string | number | symbol;
 export type Obj<T = unknown> = { [k: Key]: T };
 
-export type Mappable<T = unknown> = Obj<T> | T[];
-export type NonEmptyMappable<T = unknown> = Obj<T> | [T, ...T[]];
-export type NonEmptyTuple<T = unknown> = [T, ...T[]];
+export type Mappable<T = unknown> = Obj<T> | readonly T[];
+export type NonEmptyMappable<T = unknown> = Obj<T> | readonly [T, ...T[]];
+export type NonEmptyTuple<T = unknown> = readonly [T, ...T[]];
