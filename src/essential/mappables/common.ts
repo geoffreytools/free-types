13fa54b3758c602c6562e19302678e372508a482
geoffreyTools <geoffreyTools@users.noreply.github.com>
2023-07-27 21:03:06 +0200

    feat: male optional elements available to every mappable

--- src/essential/mappables/common.ts
+++ src/essential/mappables/common.ts
@@ -1,6 +1,6 @@
 export type Key = string | number | symbol;
 export type Obj<T = unknown> = { [k: Key]: T };
 
-export type Mappable<T = unknown> = Obj<T> | readonly T[];
-export type NonEmptyMappable<T = unknown> = Obj<T> | readonly [T, ...T[]];
+export type Mappable<T = unknown> =Partial<Obj<T> | readonly T[]>;
+export type NonEmptyMappable<T = unknown> = Partial<Obj<T> | readonly [T, ...T[]]>;
 export type NonEmptyTuple<T = unknown> = readonly [T, ...T[]];
